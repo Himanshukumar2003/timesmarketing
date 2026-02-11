c1f7533040075e8dd41bbc0744957f090cce7d1f
unknown <devliyalhimanshu@gmsil.com>
2026-02-11 15:25:27 +0530

    responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 // import { Geist, Geist_Mono } from "next/font/google";
 import PageSidebar from "@/componts/pageSidebar";
 import "./globals.css";
+import Script from "next/script";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -25,6 +26,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="r4ZZOH9QQgw_c2NbXTjvEv-ibi5VErRU-npAqD-mu5Q"
+        />
+
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-TQ78Z4WX3K"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TQ78Z4WX3K');
+          `}
+        </Script>
+      </head>
+
       <body suppressHydrationWarning>
         <PageSidebar></PageSidebar>
 
