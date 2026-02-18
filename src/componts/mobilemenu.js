88de4b1bf492efa9d821d95cc43c0189d6849a2d
unknown <devliyalhimanshu@gmsil.com>
2026-02-18 16:10:05 +0530

    dealer-locator

--- src/componts/mobilemenu.js
+++ src/componts/mobilemenu.js
@@ -113,10 +113,10 @@ const menuItems = [
       {
         id: "geared-motors",
         title: "Geared motors",
-        slug: "/sraaes/geared-motors",
+        slug: "/drives/geared-motors",
       },
-      { id: "motors", title: "Motors", slug: "/sraaes/motors" },
-      { id: "wheel-range", title: "Wheel range", slug: "/sraaes/wheel-range" },
+      { id: "motors", title: "Motors", slug: "/drives/motors" },
+      { id: "wheel-range", title: "Wheel range", slug: "/drives/wheel-range" },
     ],
   },
 ];
